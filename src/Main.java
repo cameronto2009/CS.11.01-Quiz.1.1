801c17e1f53041aa0075d07f847dbf4d3c00c623
cameron <cameront2027@student.cis.edu.hk>
2024-08-30 12:33:35 +0800

    cameron hw

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,36 @@
 import java.util.Scanner;
 
 /**
-*****************************************************************************************************
-* By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
-* “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
-*****************************************************************************************************
-*/
+ *****************************************************************************************************
+ * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge
+ * “I have neither given nor received unauthorized aid on this piece of work.”
+ * NAME: Cameron To
+ *****************************************************************************************************
+ */
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int age;
 
+        String firstName;
 
+        String favouriteFood;
 
+        System.out.println("Enter your age: ");
+
+        age = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Enter your name: ");
+
+        firstName = scanner.nextLine();
+
+        System.out.println("Enter your favourite food: ");
+
+        favouriteFood = scanner.nextLine();
+        System.out.println("Name: " + firstName + "\nAge: " + age + "\nFavourite food: " + favouriteFood);
     }
 
 }
